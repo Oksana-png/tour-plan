e4e1aeea3d37a04f93a1558ee47df6b9b8015277
Oksana-png <oksana_ovchinnikova@inbox.ru>
2021-03-10 00:09:38 +0500

    add map yandex API key

--- js/main.js
+++ js/main.js
@@ -16,6 +16,21 @@ const swiper = new Swiper('.swiper-container', {
   
 });
 
+ymaps.ready(init);
+    function init(){
+        // Создание карты.
+        var myMap = new ymaps.Map("map", {
+            // Координаты центра карты.
+            // Порядок по умолчанию: «широта, долгота».
+            // Чтобы не определять координаты центра карты вручную,
+            // воспользуйтесь инструментом Определение координат.
+            center: [57.90, 59.94],
+            // Уровень масштабирования. Допустимые значения:
+            // от 0 (весь мир) до 19.
+            zoom: 13
+        });
+    }
+
 // addEventListener('keydown', function (event) {
 //   console.log(event)
 //   if(event.key === 'ArrowRight'){
