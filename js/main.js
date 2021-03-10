a919facc4f38451e5fb4fd31051776aa65ef1f56
Oksana-png <oksana_ovchinnikova@inbox.ru>
2021-03-10 20:46:35 +0500

    create: added reviews slide and fix hotel slide

--- js/main.js
+++ js/main.js
@@ -1,21 +1,29 @@
-const swiper = new Swiper('.swiper-container', {
+const hotelSlider = new Swiper('.hotel-slider', {
   // Optional parameters
   loop: true,
   // Navigation arrows
   navigation: {
-    nextEl: '.slider-button--next',
-    prevEl: '.slider-button--prev',
+    nextEl: '.hotel-slider__button--next',
+    prevEl: '.hotel-slider__button--prev',
   },
   keyboard: {
     enabled: true,
     onlyInViewport: false,
   },
-  autoplay: {
-    delay: 6000,
-  },
+  // autoplay: {
+  //   delay: 6000,
+  // },
   
 });
 
+const reviewsSlider = new Swiper('.reviews-slider', {
+  loop: true,
+  // Navigation arrows
+  navigation: {
+    nextEl: '.reviews-slider__button--next',
+    prevEl: '.reviews-slider__button--prev',
+  }
+});
 // addEventListener('keydown', function (event) {
 //   console.log(event)
 //   if(event.key === 'ArrowRight'){
